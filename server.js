5576716f78df5aba376d56a64f61532ce67930ff
jsmithxyz <jordansmith3813@gmail.com>
2020-02-14 12:23:46 -0500

    created mysql connection, required modules

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var console_table = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,3 +15,9 @@ var connection = mysql.createConnection({
   password: "yourRootPassword",
   database: "employee_trackerDB" 
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+  });

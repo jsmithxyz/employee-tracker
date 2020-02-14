84242d4d900422f20f699fd123d3567c36e187e4
jsmithxyz <jordansmith3813@gmail.com>
2020-02-14 13:10:29 -0500

    updated seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -25,3 +25,18 @@ VALUES ("Lawyer", 90000, 3);
 
 INSERT INTO role (title, salary, department_id)
 VALUES ("Accountant", 70000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineer", 60000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 60000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 65000, 3);
+
+
+
+-- populate employees in table 'employee'
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ben", "Simmons", 1, NULL);

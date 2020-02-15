a68de25240373f6701be77fcd3b39f31807aa260
jsmithxyz <jordansmith3813@gmail.com>
2020-02-15 15:47:23 -0500

    completed 'Add/Delete functionality'

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+
+    connection.query("SELECT * from role", function (error, res) {
+        showroles = res.map(role => ({ name: role.title, value: role.id }))
+      });
+
+    connection.query("SELECT * from employee", function (error, res) {
+        showemployees = res.map(employee => ({ name: `${employee.first_name} ${employee.last_name}`, value: employee.id }))
+      });
+
     runApp();
 });
 
@@ -27,7 +36,7 @@ function runApp() {
     inquirer
         .prompt({
             name: "action",
-            type: "rawlist",
+            type: "list",
             message: "What would you like to do?",
             choices: [
                 "View All Employees",
@@ -49,7 +58,7 @@ function runApp() {
                 case "Add Employee":
                     addEmployee();
                     break;
-                case "Delete Employee":
+                case "Remove Employee":
                     deleteEmployee();
                     break;
                 case "Update Employee Role":
@@ -74,10 +83,11 @@ function allEmployees() {
         });
 }
 
+//function to view employees by department choice
 function allEmployeesByDepartment() {
     inquirer.prompt({
         name: "dept",
-        type: "rawlist",
+        type: "list",
         message: "What department would you like to view?",
         choices: [
             "Sales",
@@ -86,14 +96,78 @@ function allEmployeesByDepartment() {
             "Legal"
         ]
     })
-    .then(function (answer) {
-        connection.query(
-            "SELECT employee.id, first_name AS FIRSTNAME, last_name AS LASTNAME, title AS POSITION, name AS DEPARTMENT, salary as SALARY FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = ? ORDER BY title", [ answer.dept ], function (err, res) {
-                if (err) throw err;
-                // Log all results of the SELECT statement in table format
-                console.table('\nALL EMPLOYEES BY DEPARTMENT\n', res);
-                runApp();
-            });
-    });
+        .then(function (answer) {
+            connection.query(
+                "SELECT employee.id, first_name AS FIRSTNAME, last_name AS LASTNAME, title AS POSITION, name AS DEPARTMENT, salary as SALARY FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = ? ORDER BY title", [answer.dept], function (err, res) {
+                    if (err) throw err;
+                    // Log all results of the SELECT statement in table format
+                    console.table('\nALL EMPLOYEES BY DEPARTMENT\n', res);
+                    runApp();
+                });
+        });
 }
-  
+
+
+
+//function to add an employee
+function addEmployee() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstname",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "lastname",
+            message: "What is the employee's last name?"
+        },
+        {
+            name: "position",
+            type: "list",
+            message: "What is this employee's position?",
+            choices: showroles
+        }
+        ]).then(function (answer) {
+            console.log(answer);
+            var query = connection.query(
+                "INSERT INTO employee SET ?", 
+                {
+                    first_name: answer.firstname,
+                    last_name: answer.lastname,
+                    role_id: answer.position,
+                  },
+                function (err, res) {
+                    if (err) throw err;
+                    console.table("\nNew Employee Added Successfully!\n");
+                    runApp();
+                });
+        });
+}
+
+function deleteEmployee() {
+
+    inquirer.prompt([
+        {
+            name: "removal",
+            type: "list",
+            message: "What is this employee's position?",
+            choices: showemployees
+        }
+        ]).then(function (answer) {
+            console.log(answer);
+            var query = connection.query(
+                "DELETE FROM employee WHERE employee.id = ?", 
+                [answer.removal],
+                function (err, res) {
+                    if (err) throw err;
+                    console.table("\nEmployee Removed from Database!\n");
+                    runApp();
+                });
+        });
+}
+
+
+
+
